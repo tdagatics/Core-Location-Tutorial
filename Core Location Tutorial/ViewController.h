f3e838187cd10b8980c7512320eb3c6094b34a4d
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-24 19:35:31 -0700

    First commit of Core Location Tutorial

--- Core Location Tutorial/ViewController.h
+++ Core Location Tutorial/ViewController.h
@@ -7,9 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CLLocation *location;
+@property (weak, nonatomic) IBOutlet UILabel *coordinateLat;
+@property (weak, nonatomic) IBOutlet UILabel *coordinateLon;
+@property (weak, nonatomic) IBOutlet UILabel *altitude;
+@property (weak, nonatomic) IBOutlet UILabel *hAccuracy;
+@property (weak, nonatomic) IBOutlet UILabel *vAccuracy;
+@property (weak, nonatomic) IBOutlet UILabel *timestamp;
+@property (weak, nonatomic) IBOutlet UILabel *speed;
+@property (weak, nonatomic) IBOutlet UILabel *course;
 
 @end
 
--- Core Location Tutorial/ViewController.m
+++ Core Location Tutorial/ViewController.m
@@ -16,6 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+    self.locationManager.delegate = self;
+    self.location = [[CLLocation alloc] init];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +31,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    self.location = locations.lastObject;
+    NSLog(@"Your location was %@", self.location.description);
+    self.coordinateLat.text = [NSString stringWithFormat:@"%f", self.location.coordinate.latitude];
+    self.coordinateLon.text = [NSString stringWithFormat:@"%f", self.location.coordinate.longitude];
+    self.altitude.text = [NSString stringWithFormat:@"%f", self.location.altitude];
+    self.hAccuracy.text = [NSString stringWithFormat:@"%f", self.location.horizontalAccuracy];
+    self.vAccuracy.text = [NSString stringWithFormat:@"%f", self.location.verticalAccuracy];
+    self.timestamp.text = [NSString stringWithFormat:@"%@", self.location.timestamp];
+    self.speed.text = [NSString stringWithFormat:@"%f", self.location.speed];
+    self.course.text = [NSString stringWithFormat:@"%f", self.location.course];
+    
+}
 @end
